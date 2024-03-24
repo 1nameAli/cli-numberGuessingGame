7ffdebc184b8f34b829a642a31aa9d8f85481d93
1nameAli <149589024+1nameAli@users.noreply.github.com>
2024-03-23 19:01:15 -0700

    number guessing game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node 
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "guessedNumber",
+    type: "number",
+    message: "Enter any number :",
+  },
+]);
+// conditional check
+if (randomNumber === answer.guessedNumber) {
+  console.log("Congratulations! You have guessed the right number .");
+} else {
+  console.log("Please try again you have guessed wrong number.");
+}
